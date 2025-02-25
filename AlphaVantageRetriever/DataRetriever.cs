218e9762326edadeec45033751ccaae7d41a5141
Mark Olbert <mark@arcabama.com>
2025-02-25 15:59:51 -0800

    primary service

--- AlphaVantageRetriever/DataRetriever.cs
+++ AlphaVantageRetriever/DataRetriever.cs
@@ -1,165 +1,191 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.Json;
 using System.Threading;
-using J4JSoftware.AlphaVantageRetriever;
-using J4JSoftware.Logging;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 using ServiceStack;
-using HistoricalData = J4JSoftware.AlphaVantageRetriever.HistoricalData;
-using SecurityInfo = J4JSoftware.AlphaVantageRetriever.SecurityInfo;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
-namespace J4JSoftware.AlphaVantageRetriever
+namespace J4JSoftware.AlphaVantageRetriever;
+
+public class DataRetriever : IHostedService
 {
-    public class DataRetriever
+    private readonly Configuration _config;
+    private readonly IHostApplicationLifetime _lifetime;
+    private readonly bool _validSetup;
+    private readonly List<IParseAlphaText> _alphaParsers;
+    private readonly ILogger? _logger;
+
+    public DataRetriever(
+        Configuration config,
+        IEnumerable<IParseAlphaText> alphaParsers,
+        IHostApplicationLifetime lifetime,
+        ILoggerFactory? loggerFactory )
     {
-        private readonly object _lockObject = new object();
-        private AppConfiguration? _config;
-        private List<SecurityInfo>? _securities;
-        private int _index = 0;
-        private bool _replaceExistingPriceData;
+        _config = config;
+        _lifetime = lifetime;
+        _alphaParsers = alphaParsers.ToList();
 
-        public DataRetriever( 
-            AlphaVantageContext dbContext,
-            IJ4JLogger logger )
+        _logger = loggerFactory?.CreateLogger(GetType());
+        _validSetup = CheckParsers();
+    }
+
+    private bool CheckParsers()
+    {
+        var retVal = true;
+
+        foreach( var dataset in Enum.GetValues<AlphaVantageData>() )
         {
-            DbContext = dbContext;
-            
-            Logger = logger;
-            Logger.SetLoggedType( this.GetType() );
-        }
-
-        protected IJ4JLogger Logger { get; }
-        protected AlphaVantageContext DbContext { get; }
-
-        protected string ApiKey => _config?.ApiKey ?? "";
-
-        public int ReportingYear => _config?.ReportingYear ?? -1;
-
-        public void Initialize( AppConfiguration config, bool replaceExistingPriceData )
-        {
-            _config = config;
-
-            _securities = DbContext.Securities.ToList();
-            _index = 0;
-            _replaceExistingPriceData = replaceExistingPriceData;
-        }
-
-        public void ProcessNextSymbol( object? stateInfo )
-        {
-            // wrap the call to the actual processing method in a monitor to
-            // prevent multiple simultaneous accesses to the DbContext object
-            
-            if( Monitor.TryEnter( _lockObject ) )
+            switch( _alphaParsers.Count( p => p.SupportsFunction( dataset ) ) )
             {
-                try
-                {
-                    // check to ensure we were given the AutoResetEvent we're expecting
-                    var jobDone = stateInfo as AutoResetEvent;
+                case 0:
+                    _logger?.NoParser(dataset.ToString());
+                    retVal = false;
 
-                    if( jobDone == null )
-                        Logger.Error( $"Argument is not a {nameof( AutoResetEvent )}" );
-                    else Process( jobDone );
-                }
-                finally
-                {
-                    Monitor.Exit(_lockObject);
-                }
+                    break;
+
+                case 1:
+                    // no op; expected case
+                    break;
+
+                default:
+                    _logger?.MultipleParsers( dataset.ToString() );
+                    retVal = false;
+
+                    break;
             }
         }
 
-        protected void Process( AutoResetEvent jobDone )
+        return retVal;
+    }
+
+    public async Task StartAsync(CancellationToken ctx)
+    {
+        if( !_validSetup )
         {
-            var mesg = new StringBuilder();
-
-            SecurityInfo? dbSecurity = null;
-
-            // scan through all the SecurityInfo objects looking for the next one that
-            // is reportable, has a ticker and hasn't already had its data retrieved
-            while( true )
-            {
-                if( _index >= _securities!.Count )
-                {
-                    jobDone.Set();
-                    return;
-                }
-
-                // the StringBuilder instance 'mesg', in addition to holding any error
-                // messages to report, is also the flag we use to tell that we've found
-                // a SecurityInfo to process. that's done by checking to see if there are
-                // no messages.
-                mesg.Clear();
-
-                dbSecurity = _securities[ _index ];
-
-                if( !dbSecurity.Reportable )
-                    mesg.Append( "not reportable" );
-
-                bool emptyTicker = String.IsNullOrEmpty( dbSecurity.Ticker );
-
-                if( emptyTicker )
-                {
-                    if( mesg.Length > 0 ) mesg.Append( "; " );
-                    mesg.Append( "has no ticker symbol" );
-                }
-
-                if( dbSecurity.RetrievedData && _replaceExistingPriceData )
-                {
-                    if( mesg.Length > 0 ) mesg.Append( "; " );
-                    mesg.Append( "price data already retrieved" );
-                }
-
-                // if no message we've found a symbol to process
-                if( mesg.Length == 0 ) break;
-
-                mesg.Insert( 0, $"{dbSecurity.Issuer} " );
-                Logger.Information( mesg.ToString() );
-
-                _index++;
-            }
-
-            // retrieve historical price data
-            var url =
-                $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={dbSecurity.Ticker}&outputsize=full&apikey={ApiKey}&datatype=csv";
-            var rawText = url.GetStringFromUrl();
-
-            List<AlphaVantageData>? priceData = null;
-
-            try
-            {
-                priceData = rawText.FromCsv<List<AlphaVantageData>>();
-            }
-            catch(Exception e)
-            {
-                dbSecurity.ErrorMessage = e.Message + "; " + rawText;
-                DbContext.SaveChanges();
-
-                Logger.Error( $"Exception for {dbSecurity.Issuer}: {dbSecurity.ErrorMessage}" );
-                _index++;
-                
-                return;
-            }
-
-            Logger.Information<string, int>( "{0}: {1} lines retrieved", dbSecurity.Ticker, priceData.Count );
-
-            DbContext.HistoricalData.AddRange( priceData
-                .Where( pd => pd.Timestamp.Year == ReportingYear )
-                .Select( pd => new HistoricalData()
-                {
-                    Close = pd.Close,
-                    High = pd.High,
-                    Low = pd.Low,
-                    Open = pd.Open,
-                    Timestamp = pd.Timestamp,
-                    SecurityInfo = dbSecurity,
-                    Volume = pd.Volume
-                } ) );
-
-            dbSecurity.RetrievedData = true;
-
-            DbContext.SaveChanges();
-
-            _index++;
+            _logger?.InvalidSetup();
+            _lifetime.StopApplication();
+            return;
         }
+
+        if (_config.Tickers.Count == 0)
+        {
+            _logger?.NoTickersDefined();
+            _lifetime.StopApplication();
+            return;
+        }
+
+        if ( string.IsNullOrEmpty( _config.UserConfiguration.EncryptedApiKey ) && string.IsNullOrEmpty( _config.ApiKey ) )
+        {
+            _logger?.NoApiKey();
+            _lifetime.StopApplication();
+            return;
+        }
+
+        if( _config.DataToRetrieve.Count == 0 )
+        {
+            _config.DataToRetrieve.Add( AlphaVantageData.Ticker );
+            _logger?.NoDataSets();
+        }
+
+        _logger?.RetrievalStarted();
+
+        foreach (var avData in _config.DataToRetrieve)
+        {
+            var parser = _alphaParsers.FirstOrDefault(r => r.SupportsFunction(avData));
+
+            if (parser == null)
+            {
+                _logger?.NoParser( avData.ToString() );
+                continue;
+            }
+
+            if( !parser.TryGetAccessAllowed( avData, out var isAllowed ) || !isAllowed )
+            {
+                _logger?.PremiumAccountRequired( avData.ToString() );
+                continue;
+            }
+
+            _logger?.RetrievingFunction(avData.ToString());
+
+            await OutputFile( avData, parser.GetData( avData, ctx ), ctx );
+        }
+
+        _logger?.RetrievalComplete();
+
+        _lifetime.StopApplication();
+    }
+
+    private async Task OutputFile<T>(AlphaVantageData avData, IAsyncEnumerable<T> asyncData, CancellationToken ctx)
+        where T : class
+    {
+        var path = Path.GetDirectoryName(_config.OutputFilePath) ?? Environment.CurrentDirectory;
+        var fileName = Path.GetFileName(_config.OutputFilePath) ?? "AlphaVantage";
+        var woExt = Path.GetFileNameWithoutExtension(fileName);
+
+        var revisedFileName = $"{woExt}-{avData.ToString()}.{_config.OutputFormat switch
+        {
+            OutputFormat.Json => "json",
+            _ => "csv"
+        }}";
+
+        var filePath = Path.Combine(path, revisedFileName);
+
+        if (File.Exists(filePath))
+            File.Delete(filePath);
+
+        var data = await asyncData.ToListAsync( ctx );
+
+        switch (_config.OutputFormat)
+        {
+            case OutputFormat.Json:
+                _logger?.OutputFileStarted( revisedFileName );
+
+                var options = new JsonSerializerOptions { WriteIndented = true };
+
+                await File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(data, options), ctx);
+
+                _logger?.OutputFileCompleted();
+
+
+                break;
+
+            case OutputFormat.Csv:
+                _logger?.OutputFileStarted(revisedFileName);
+
+                await File.WriteAllTextAsync(filePath, data.ToCsv(), ctx);
+
+                _logger?.OutputFileCompleted();
+
+                break;
+
+            default:
+                _logger?.LogError("Unsupported output style {style}", _config.OutputFormat.ToString());
+                break;
+        }
+    }
+
+    public async Task StopAsync( CancellationToken ctx )
+    {
+        await Log.CloseAndFlushAsync();
+
+        var userConfigPath = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ),
+                                           "J4JSoftware",
+                                           nameof( AlphaVantageRetriever ),
+                                           "userConfig.json" );
+
+        if( File.Exists( userConfigPath ) )
+            File.Delete( userConfigPath );
+
+        var options = new JsonSerializerOptions { WriteIndented = true };
+
+        await File.WriteAllTextAsync( userConfigPath,
+                                      JsonSerializer.Serialize( _config.UserConfiguration, options ),
+                                      ctx );
     }
 }
