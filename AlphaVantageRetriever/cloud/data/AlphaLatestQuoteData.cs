bf0973b38df59a6fcceed359c763920a78eff2c0
Mark Olbert <mark@arcabama.com>
2025-02-25 15:56:39 -0800

    structures for parsing AlphaVantage query results

---
+++ AlphaVantageRetriever/cloud/data/AlphaLatestQuoteData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace J4JSoftware.AlphaVantageRetriever;
+
+#pragma warning disable 8618
+public class AlphaLatestQuoteData
+{
+    public string Symbol { get; set; }
+    public decimal Open { get; set; }
+    public decimal High { get; set; }
+    public decimal Low { get; set; }
+    public decimal Price { get; set; }
+    public decimal Volume { get; set; }
+    public DateTime LatestDay { get; set; }
+    public decimal PreviousClose { get; set; }
+    public decimal Change { get; set; }
+    public decimal ChangePercent { get; set; }
+}
---
+++ AlphaVantageRetriever/cloud/data/AlphaSymbolInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace J4JSoftware.AlphaVantageRetriever;
+
+public record AlphaSymbolInfo( string Symbol, string Name, string Region, string Currency, decimal MatchScore)
+{
+    public sealed class SymbolEqualityComparer : IEqualityComparer<AlphaSymbolInfo>
+    {
+        public bool Equals( AlphaSymbolInfo? x, AlphaSymbolInfo? y )
+        {
+            if( ReferenceEquals( x, y ) )
+                return true;
+            if( x is null )
+                return false;
+            if( y is null )
+                return false;
+
+            return x.GetType() == y.GetType() && string.Equals( x.Symbol, y.Symbol, StringComparison.OrdinalIgnoreCase );
+        }
+
+        public int GetHashCode( AlphaSymbolInfo obj )
+        {
+            return StringComparer.OrdinalIgnoreCase.GetHashCode( obj.Symbol );
+        }
+    }
+
+    public PriceEntry? LatestPrice { get; set; }
+    public List<PriceEntry> HistoricalPrices { get; } = [];
+}
